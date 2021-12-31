619a3600874def3169c05961339a75735b72f77e
Jeonghwan-Choi <yzon007@naver.com>
2021-12-31 16:13:15 +0900

    RESTful Service

---
+++ demo/src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.HelloWorldBean;
+
+@RestController
+public class HelloWorldController {
+
+	
+	//@RequestMapping(method=RequestMethod.GET, path="hello-world")
+	@GetMapping(path = "hello-world")
+	public String HelloWorld() {
+		
+		return "Hello World";
+		
+	}
+	
+	@GetMapping(path = "hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		
+		return new HelloWorldBean("Hello World");
+		
+	}
+	
+	@GetMapping(path = "hello-world-bean/path-variable/{name}")
+	public HelloWorldBean helloWorldBean(@PathVariable String name) {
+		
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+		
+	}
+	
+}

0a8f5d84c15772ba233354db1b8a2f4a2ab37570
Jeonghwan-Choi <yzon007@naver.com>
2022-01-01 23:50:29 +0900

    hello update

--- demo/src/main/java/com/example/demo/HelloWorldController.java
+++ demo/src/main/java/com/example/demo/HelloWorldController.java
@@ -3,12 +3,7 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.HelloWorldBean;
-
 @RestController
 public class HelloWorldController {
 
